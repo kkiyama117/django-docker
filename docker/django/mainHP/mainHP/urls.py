a5f0a0e2b41ca880884a7dd214c8050102456507
kkiyama117 <k.kiyama117@gmail.com>
2018-03-27 21:17:51 +0900

    Add some model for transfacils

--- docker/django/mainHP/mainHP/urls.py
+++ docker/django/mainHP/mainHP/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('main.urls')),
+    path('trans', include('transfacils.urls')),
     path('admin/', admin.site.urls),
 ]
