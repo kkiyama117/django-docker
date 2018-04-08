39fa696d7c3fdb3e4cda7d7ba5d843987a42758b
kkiyama117 <k.kiyama117@gmail.com>
2018-04-09 03:04:09 +0900

    Modify scripts

--- django/init.sh
+++ django/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py makemigrations
-python manage.py migrate
 python manage.py initializer
+python manage.py migrate
 python manage.py loaddata initial_data.json
